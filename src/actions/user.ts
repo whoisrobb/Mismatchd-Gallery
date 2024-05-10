1433828b266c2cbb8ad0503271f647da901eed60
Muchiri <developedbyrobbie@gmail.com>
2024-05-10 23:50:33 +0300

    feat: Persist user data to database using clerk webhooks

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -2,7 +2,41 @@
 
 import db from "@/db/drizzle";
 import { UserTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
+type SavedUser = {
+    username: string;
+    email: string;
+    avatar: string | null;
+}
+
+// CREATE NEW USER
+export const saveOrUpdateUser = async (userData: SavedUser) => {
+    try {
+        const { email } = userData;
+
+        const user = await db.select()
+            .from(UserTable)
+            .where(eq(UserTable.email, email))
+
+        if (user.length > 0) {
+            await db.update(UserTable)
+                .set(userData)
+                .where(eq(UserTable.email, email))
+        } else {
+            await db.insert(UserTable)
+                .values(userData)
+        }
+
+        const savedUser = await db.select()
+            .from(UserTable)
+            .where(eq(UserTable.email, email))
+        
+        return savedUser;
+    } catch (err) {
+        console.error(err)
+    }
+}
 
 // GET ALL USERS
 export const getAllUsers = async () => {
