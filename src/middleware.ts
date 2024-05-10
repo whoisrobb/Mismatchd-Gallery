05c9814b0f40eaa2c8982cf0a2d112b738daee72
Muchiri <developedbyrobbie@gmail.com>
2024-05-10 23:18:21 +0300

    feat: Add auth functionality with clerk

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+    // TODO: Add protected routes
+  ]);
+   
+  export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
+  });
+
+export const config = {
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
