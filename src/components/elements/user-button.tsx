599f1054df9cac73f50396b7beb4aa5d837b66ab
Muchiri <developedbyrobbie@gmail.com>
2024-05-12 15:54:16 +0300

    feat: Add user button to header

---
+++ src/components/elements/user-button.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+// import { SignOutButton, useUser } from "@clerk/clerk-react";
+// import { Link, useNavigate } from "react-router-dom";
+import { DashboardIcon, ExitIcon, GearIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { SignOutButton, useUser } from "@clerk/nextjs";
+
+const UserButton = () => {
+    // const navigate = useNavigate();
+    const { user } = useUser();
+  return (
+    <DropdownMenu>
+        <DropdownMenuTrigger>
+            <Avatar>
+                <AvatarImage src={user?.imageUrl} />
+                <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+            <DropdownMenuLabel>
+                {user?.firstName ? <p className="">{user.firstName}</p> : <p className="">{user?.username}</p> }
+                <p className="text-muted-foreground">{user?.emailAddresses[0].emailAddress}</p>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+                <Link href={'/admin/account'} className="flex items-center space-x-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                        <path d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z" />
+                    </svg>
+                    <p>Profile</p>
+                </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <Link href={'/admin/stores'} className="flex items-center space-x-2">
+                    <DashboardIcon />
+                    <p>Dashboard</p>
+                </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+                <Link href={'/admin/settings'} className="flex items-center space-x-2">
+                    <GearIcon/>
+                    <p>Settings</p>
+                </Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="space-x-2">
+                <ExitIcon />
+                <SignOutButton />
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
+
+  )
+}
+
+export default UserButton;
--- src/components/shells/site-header.tsx
+++ src/components/shells/site-header.tsx
@@ -1,6 +1,9 @@
-import { UserButton } from '@clerk/nextjs';
+// import { UserButton } from '@clerk/nextjs';
 import Link from 'next/link';
 import React, { ReactNode } from 'react';
+import UserButton from '../elements/user-button';
+import { Button } from '../ui/button';
+import { SignedIn, SignedOut } from '@clerk/nextjs';
 
 interface SiteHeaderProps {
     children: ReactNode
@@ -11,7 +14,19 @@ const SiteHeader = ({ children }: SiteHeaderProps) => {
     <header className='flex justify-between items-center border-b h-16'>
         <Link href={'/'} className='font-bold text-xl'>Mismatchd</Link>
         { children }
-        <UserButton />
+        <div className="flex items-center gap-2">                
+            <div className="hidden lg:block md:block">
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+
+            <SignedOut>
+              <Link href={'/sign-in'}>
+                <Button>Sign in</Button>
+              </Link>
+            </SignedOut>
+        </div>
     </header>
   )
 }
