af70d214b9c717daa3b196c90579798a0dacd599
Muchiri <developedbyrobbie@gmail.com>
2024-05-14 22:17:12 +0300

    feat: Add cart items to cart sheet

--- src/components/cart/cart-sheet.tsx
+++ src/components/cart/cart-sheet.tsx
@@ -10,23 +10,35 @@ import {
     SheetTrigger,
   } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
+import { CartProduct, useCart } from "./cart-provider";
+import Image from "next/image";
+import { formatCurrency } from "@/lib/utils";
+import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
+import { ScrollArea } from "@radix-ui/react-scroll-area";
   
 const CartSheet = () => {
+    const { cartQuantity, cartItems } = useCart();
   return (
     <Sheet>
         <SheetTrigger>
             <Button asChild size={'icon'} variant={'ghost'} className="relative">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                </svg>
-                {/* {cartQuantity > 0 && <span className="absolute rounded-full bg-red-600 h-5 w-5 bottom-[-.5rem] right-[-.5rem] text-sm text-[#fff] flex items-center justify-center">{JSON.stringify(cartQuantity)}</span>} */}
+                <div className="">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                    </svg>
+                    {cartQuantity > 0 && <span className="absolute rounded-full bg-red-600 h-5 w-5 bottom-[-.5rem] right-[-.5rem] text-sm text-[#fff] flex items-center justify-center">{JSON.stringify(cartQuantity)}</span>}
+                </div>
             </Button>
         </SheetTrigger>
         <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
             <SheetHeader>
                 <SheetTitle>Shopping Cart</SheetTitle>
-                <SheetDescription>
-                    Content goes here
+                <SheetDescription className="h-[calc(100vh-6rem)] overflow-y-scroll pr-10 test">
+                    {cartItems && cartItems.length > 0 ?
+                        cartItems.map((item) => (
+                            <CartItems item={item} />
+                        ))
+                    : null}
                 </SheetDescription>
             </SheetHeader>
         </SheetContent>
@@ -35,3 +47,35 @@ const CartSheet = () => {
 }
 
 export default CartSheet;
+
+const CartItems = ({ item }: { item: CartProduct }) => {
+    const { decreaseQuantity, increaseQuantity } = useCart();
+
+    return (
+        <div className="grid grid-cols-2 gap-2 border-b py-2 items-start">
+            <div className="">
+                <Image
+                    src={item.images![0]}
+                    height={100}
+                    width={100}
+                    alt=""
+                />
+            </div>
+            <div className="">
+                <p className="">{item.name}</p>
+                <p className="">{formatCurrency(Number(item.price))}</p>
+                <div className="flex justify-between">
+                    <div className="flex items-center gap-2">
+                        <Button variant={'outline'} size={'icon'} className="" onClick={() => decreaseQuantity(item.productId)}><MinusIcon /></Button>
+                        <div className="">{item.quantity}</div>
+                        <Button variant={'outline'} size={'icon'} className="" onClick={() => increaseQuantity(item.productId)}><PlusIcon /></Button>
+                    </div>
+                    <div className="">
+                        <p className="">Total</p>
+                        <p className="text-primary font-bold">{formatCurrency(item.quantity * Number(item.price))}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
