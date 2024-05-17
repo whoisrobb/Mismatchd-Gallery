0e31838f718690d0e471efd8875aee739b734a48
Muchiri <developedbyrobbie@gmail.com>
2024-05-17 23:10:13 +0300

    refactor: style login pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+  return (
+    <div className="w-full h-screen grid place-items-center">
+      <SignIn path="/sign-in" />
+    </div>
+  );
 }
